c7084ba5956f6b01489d82b0cda1daef9729c4ce
ashurmuhammad <aabddev@gmail.com>
2022-02-16 15:32:17 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_getx/services/theme_service.dart';
+import 'package:flutter_todo_getx/ui/home_page.dart';
+import 'package:flutter_todo_getx/ui/theme.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_storage/get_storage.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+        theme: Themes.light,
+        darkTheme: Themes.dark,
+        themeMode: ThemeService().theme,
+      home: HomePage()
+    );
+  }
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_getx/services/notification_service.dart';
+import 'package:flutter_todo_getx/services/theme_service.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var notifyHelper;
+  @override
+  void initState() {
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
+    notifyHelper.requestIOSPermissions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      leading: GestureDetector(
+        onTap: () {
+          ThemeService().switchTheme();
+         notifyHelper.requestIOSPermissions();
+         notifyHelper.displayNotification(
+           title: "ThemeChanged",
+           body: Get.isDarkMode?" lightMode":"darkMode"
+         );
+         notifyHelper.scheduledNotification();
+        },
+        child: Icon(
+          Icons.nightlight_round,
+          size: 20,
+        ),
+      ),
+      actions: [
+        Icon(
+          Icons.person,
+          size: 20,
+        ),
+        SizedBox(
+          width: 20,
+        )
+      ],
+    );
+  }
+}
